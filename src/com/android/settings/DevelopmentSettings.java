1277e7d631608d192dbda5590c5829241e77eca2
beanstown106 <nbenis106@gmail.com>
2015-10-14 12:35:49 -0400

    settings: Enable Development Settings By Default
    Change-Id: Ib07e32e27a3a93cfcf80dd7b68d3beea4a4c2302
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -1921,7 +1921,9 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
                     return context.getSharedPreferences(DevelopmentSettings.PREF_FILE,
                             Context.MODE_PRIVATE).getBoolean(
                                     DevelopmentSettings.PREF_SHOW,
-                                    android.os.Build.TYPE.equals("eng"));
+                                    android.os.Build.TYPE.equals("eng") ||
+                                    android.os.Build.TYPE.equals("userdebug") ||
+                                    android.os.Build.TYPE.equals("user"));
                 }
 
                 @Override
