04cb64bf33371d589c784b2bb2bd94b047c828cc
t-rex600 <fisch890@gmail.com>
2016-01-17 16:49:05 +0100

    update Navbar Duplicate field

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -162,14 +162,6 @@ public final class Utils {
     private static final int SECONDS_PER_HOUR = 60 * 60;
     private static final int SECONDS_PER_DAY = 24 * 60 * 60;
 
-	// Device types
-    private static final int DEVICE_PHONE = 0;
-    private static final int DEVICE_HYBRID = 1;
-    private static final int DEVICE_TABLET = 2;
-
-    // Device type reference
-    private static int sDeviceType = -1;
-
     public static final String OS_PKG = "os";
 
     private static SparseArray<Bitmap> sDarkDefaultUserBitmapCache = new SparseArray<Bitmap>();
@@ -661,40 +653,6 @@ public final class Utils {
     public static boolean isPackageInstalled(Context context, String pkg) {
         return isPackageInstalled(context, pkg, true);
 
-    }
-
-    private static int getScreenType(Context context) {
-        if (sDeviceType == -1) {
-            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-           DisplayInfo outDisplayInfo = new DisplayInfo();
-            wm.getDefaultDisplay().getDisplayInfo(outDisplayInfo);
-            int shortSize = Math.min(outDisplayInfo.logicalHeight, outDisplayInfo.logicalWidth);
-            int shortSizeDp = shortSize * DisplayMetrics.DENSITY_DEFAULT
-                    / outDisplayInfo.logicalDensityDpi;
-            if (shortSizeDp < 600) {
-                // 0-599dp: "phone" UI with a separate status & navigation bar
-                sDeviceType =  DEVICE_PHONE;
-            } else if (shortSizeDp < 720) {
-                // 600-719dp: "phone" UI with modifications for larger screens
-                sDeviceType = DEVICE_HYBRID;
-            } else {
-                // 720dp: "tablet" UI with a single combined status & navigation bar
-                sDeviceType = DEVICE_TABLET;
-            }
-        }
-        return sDeviceType;
-    }
-
-    public static boolean isPhone(Context context) {
-        return getScreenType(context) == DEVICE_PHONE;
-    }
-
-    public static boolean isHybrid(Context context) {
-        return getScreenType(context) == DEVICE_HYBRID;
-    }
-
-    public static boolean isTablet(Context context) {
-        return getScreenType(context) == DEVICE_TABLET;
     }
 
     /**
